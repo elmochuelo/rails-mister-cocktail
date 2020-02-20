814232100ba7a0320b5dbe8974fc78c0d3ec6520
David Wise <davidmarkwise@gmail.com>
2020-02-20 22:18:36 +0100

    Revisions to controller files

--- app/controllers/cocktail_controller.rb
+++
@@ -1,8 +0,0 @@
-class CocktailController < ApplicationController
-  def index
-    @cocktails = Cocktail.all
-  end
-
-  def details
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'cocktail/list'
   get 'cocktail/details'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
